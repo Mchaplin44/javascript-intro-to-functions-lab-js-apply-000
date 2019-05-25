ae18ab25d8449a0898ef4a58346c5ec5459f86b1
Martin Chaplin <martin.chaplin44@gmail.com>
2019-05-25 03:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log('hello')
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string){
   return "I can't hear you!";
-
+}
 if (string.toUpperCase() === string){
   return "YES INDEED";
 }
